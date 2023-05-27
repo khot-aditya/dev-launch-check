4bb28fb95e40ca8959f4f972b1810aeb8bc2d8b3
Aditya Khot <adityakhot6838@gmail.com>
2023-05-27 11:44:25 +0530

    added background and github stars, minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Home } from './components/pages/home';
 function App() {
 
   return (
-    <div className="bg-base-100 h-100 background">
+    <div className="bg-base-100 h-100 background-pattern">
       <Home />
     </div>
   );
